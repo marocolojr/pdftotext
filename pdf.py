59484d7d9d9d3c39984ce0fad05c684215b2de82
marocolojr <marocolojr@gmail.com>
2023-03-27 11:58:11 -0300

    minor changed

--- pdf.py
+++ pdf.py
@@ -1,14 +1,10 @@
 import PyPDF2
 
-
 with open('C:\\Users\\maroc\\Documentos\\CODE\\pdftotext\\DA.pdf', 'rb') as pdf_file:
 
-
     pdf_reader = PyPDF2.PdfReader(pdf_file)
-
-   
+  
     num_pages = len(pdf_reader.pages)
-
 
     text = ''
     for page in pdf_reader.pages:
